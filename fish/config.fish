7a28b14cb5a2c578922382c90608be910ef7a492
Eunchong Yu <kroisse@gmail.com>
2013-08-01 16:27:42 +0900

    Apply oh-my-fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,36 @@
 set SCRIPT_PATH (dirname (status -f))
 
-if not set -q FISH_LOCAL_CONFIG
-    set -g FISH_LOCAL_CONFIG "$HOME/.config/fish/config.local.fish"
+if not set -q fish_local_config
+    set -g fish_local_config "$HOME/.config/fish/config.local.fish"
 end
 
 set -g EDITOR 'emacsclient --alternate-editor=vim'
+
+
+### oh-my-fish configs
+
+# Path to your oh-my-fish.
+set fish_path $SCRIPT_PATH/oh-my-fish
+
+# Theme
+set fish_theme agnoster
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+set fish_plugins brew
+
+# Path to your custom folder (default path is $FISH/custom)
+set fish_custom $SCRIPT_PATH/custom
+
+# Load oh-my-fish cofiguration.
+. $fish_path/oh-my-fish.fish
+
 
 if [ -f "$SCRIPT_PATH/autoenv/autoenv.fish" ]
     . "$SCRIPT_PATH/autoenv/autoenv.fish"
 end
 
-if [ -f $FISH_LOCAL_CONFIG ]
-    . $FISH_LOCAL_CONFIG
+if [ -f $fish_local_config ]
+    . $fish_local_config
 end
