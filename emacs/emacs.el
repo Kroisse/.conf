362293ce1cbc4368f0d6fc88294fbf4d2e0e59b7
Eunchong Yu <kroisse@gmail.com>
2014-08-18 16:04:39 +0900

    Several Emacs customize things

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -47,14 +47,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-fuzzy-cursor-color "")
+ '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(column-number-mode t)
  '(css-indent-offset 4)
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(desktop-path (quote ("~")))
  '(desktop-save-mode t)
+ '(fci-rule-color "#eee8d5")
  '(fill-column 80)
  '(fringe-mode (quote (0)) nil (fringe))
+ '(global-auto-complete-mode t)
+ '(global-flycheck-mode t nil (flycheck))
+ '(google-this-mode t)
  '(help-at-pt-display-when-idle (quote (keymap local-map button kbd-help flymake-overlay)) nil (help-at-pt))
  '(help-at-pt-timer-delay 0.8)
+ '(highlight-tail-colors (quote (("#eee8d5" . 0) ("#B4C342" . 20) ("#69CABF" . 30) ("#69B7F0" . 50) ("#DEB542" . 60) ("#F2804F" . 70) ("#F771AC" . 85) ("#eee8d5" . 100))))
  '(indent-tabs-mode nil)
  '(initial-buffer-choice "~/workspace")
  '(js-indent-level 4)
@@ -80,6 +89,9 @@
  '(term-exec-hook (quote (set-buffer-process-coding-system-utf-8-unix)))
  '(tool-bar-mode nil)
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
+ '(vc-annotate-background "#586e75")
+ '(vc-annotate-color-map (quote ((20 . "#990A1B") (40 . "#FF6E64") (60 . "#cb4b16") (80 . "#7B6000") (100 . "#b58900") (120 . "#DEB542") (140 . "#546E00") (160 . "#859900") (180 . "#B4C342") (200 . "#3F4D91") (220 . "#6c71c4") (240 . "#9EA0E5") (260 . "#2aa198") (280 . "#69CABF") (300 . "#00629D") (320 . "#268bd2") (340 . "#69B7F0") (360 . "#d33682"))))
+ '(vc-annotate-very-old-color "#93115C")
  '(x-select-enable-clipboard t)
  '(zencoding-indentation 2))
 
@@ -89,7 +101,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(flymake-errline ((t (:underline "red"))))
- '(flymake-infoline ((t (:underline "green"))))
+ '(flymake-infoline ((t (:underline "green"))) t)
  '(flymake-warnline ((t (:underline "yellow")))))
 
 ;;;; user-defined settings
